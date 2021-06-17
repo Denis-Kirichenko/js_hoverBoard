c5013bb261714e5d43dd93674228e8f158cacb77
Denis <dehicka9292@gmail.com>
2021-06-17 15:55:23 +0300

    small changes in JS

--- script.js
+++ script.js
@@ -16,27 +16,24 @@ const squaresNumber = Math.floor(height / 20) * Math.floor(width / 20);
 for (let i = 0; i < squaresNumber; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
-  square.addEventListener('mouseover', () => {
-    setColor(square);
-  });
-  square.addEventListener('mouseleave', () => {
-    removeColor(square);
-  });
+  square.addEventListener('mouseover', setColor);
+  square.addEventListener('mouseleave', removeColor);
   board.append(square);
 }
 
-function setColor(elem) {
+function setColor(event) {
+  elem = event.target;
   const color = getRandomColor();
   elem.style.backgroundColor = color;
   elem.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function removeColor(elem) {
+function removeColor(event) {
+  elem = event.target;
   elem.style.backgroundColor = '#1d1d1d';
   elem.style.boxShadow = `0 0 2px #1d1d1d`;
 }
 
 function getRandomColor() {
-  const index = Math.floor(Math.random() * colors.length - 1);
-  return colors[index];
+  return colors[Math.floor(Math.random() * colors.length - 1)];
 }
